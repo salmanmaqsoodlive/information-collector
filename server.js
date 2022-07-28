45d41938a5339f024c3b2880f604ba2942ed09de
salman <mac@macs-MacBook-Pro.local>
2022-07-28 13:06:19 +0500

    setup the automatic deployment to heroku

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const _port = 8080;
+const _app_folder = "dist/hello-world";
+const app = express();
+
+// ---- SERVE STATIC FILES ---- //
+app.get("*.*", express.static(_app_folder, { maxAge: "1y" }));
+
+// ---- SERVE APLICATION PATHS ---- //
+app.all("*", function (req, res) {
+  res.status(200).sendFile(`/`, { root: _app_folder });
+});
+
+// ---- START UP THE NODE SERVER  ----
+app.listen(_port, function () {
+  console.log(
+    "Node Express server for " +
+      app.name +
+      " listening on http://localhost:" +
+      _port
+  );
+});
